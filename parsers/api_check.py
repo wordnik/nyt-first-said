2c6644bcbe8cfa54ca598d4706c6092186c49c06
maxbittker <maxbittker@gmail.com>
2017-12-17 14:56:18 -0500

    429 retry

--- parsers/api_check.py
+++ parsers/api_check.py
@@ -9,9 +9,11 @@ key = ""
 def check_api(word):
     query_string = { 'api-key': key, 'q': word}
     req = requests.get('https://api.nytimes.com/svc/search/v2/articlesearch.json', params=query_string)
-    if req.status_code in set([529,504]):
-        time.sleep(15)
+    if req.status_code in set([429, 529, 504]):
+        time.sleep(25)
         return check_api(word)
     result = req.json()
+    if req.status_code == 500:
+        return False 
     numResults = len(result['response']['docs'])
     return numResults < 2
