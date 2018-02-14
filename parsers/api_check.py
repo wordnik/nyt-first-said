360bb4306b5387e548729551b70ffde2dc8a8a76
maxbittker <maxbittker@gmail.com>
2018-02-14 10:20:22 -0800

    fixes NYT-NEWWORD-854

--- parsers/api_check.py
+++ parsers/api_check.py
@@ -9,11 +9,14 @@ key = ""
 def check_api(word):
     query_string = { 'api-key': key, 'q': word}
     req = requests.get('https://api.nytimes.com/svc/search/v2/articlesearch.json', params=query_string)
+    
     if req.status_code in set([429, 529, 504]):
         time.sleep(25)
         return check_api(word)
-    result = req.json()
+    
     if req.status_code == 500:
         return False 
+
+    result = req.json()
     num_results = len(result['response']['docs'])
     return num_results < 2
