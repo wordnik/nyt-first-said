6a28cb2f866d874aa064ef4ede499271f3c36c96
Jim Kang <jimkang@gmail.com>
2025-08-11 12:21:22 -0400

    Add validation, bulk upload success check, more error detail.

--- js/routes/sentences-to-elastic.js
+++ js/routes/sentences-to-elastic.js
@@ -18,87 +18,100 @@ const elasticBaseURL =
   'https://es-examples-042023.es.vpce.us-west-1.aws.elastic-cloud.com';
 
 async function handler(event) {
-  const { bucket, key } = parseEvent(event);
-  var s3Client = new S3Client();
-
-  var buffer;
+  // console.log('event:', JSON.stringify(event, null, 2));
 
   try {
-    buffer = await streamFromBucketToMemory({
-      s3Client,
-      bucketName: bucket.name,
-      key,
-      allowedBodyContentTypes,
-    });
-  } catch (error) {
-    const message = `Error getting object ${key} from bucket ${bucket.name}.`;
-    throw new VError(error, message);
-  }
+    const { bucket, key } = parseEvent(event);
+    var s3Client = new S3Client();
 
-  var sentenceObj = [];
+    var buffer;
 
-  try {
-    const fileContents = buffer.toString('utf8');
-    sentenceObj = JSON.parse(fileContents);
-  } catch (error) {
-    const message = `Error reading contents of ${key}.`;
-    throw new VError(error, message);
-  }
+    try {
+      buffer = await streamFromBucketToMemory({
+        s3Client,
+        bucketName: bucket.name,
+        key,
+        allowedBodyContentTypes,
+      });
+    } catch (error) {
+      const message = `Error getting object ${key} from bucket ${bucket.name}.`;
+      throw new VError(error, message);
+    }
 
-  var { error } = esExampleSentenceSchema.validate(sentenceObj);
-  if (error) {
-    throw new VError(error, 'Sentence object failed validattion.');
-  }
+    var sentenceObj = [];
 
-  const authValues = 'ApiKey ' + elasticAPIKey;
+    try {
+      const fileContents = buffer.toString('utf8');
+      sentenceObj = JSON.parse(fileContents);
+    } catch (error) {
+      const message = `Error reading contents of ${key}.`;
+      throw new VError(error, message);
+    }
 
-  try {
-    const bulkInitURL = elasticBaseURL + '/examples-epub-020225/_open';
-    let res = await fetch(bulkInitURL, {
-      headers: { Authorization: authValues },
-      method: 'POST',
-    });
-    if (!res.ok) {
-      throw new Error(
-        `Failure response to open elastic bulk posting: ${res.status}/${res.statusText}`
-      );
+    var { error } = esExampleSentenceSchema.validate(sentenceObj);
+    if (error) {
+      throw new VError(error, 'Sentence object failed validattion.');
+    }
+
+    const authValues = 'ApiKey ' + elasticAPIKey;
+
+    try {
+      const bulkInitURL = elasticBaseURL + '/examples-epub-020225/_open';
+      let res = await fetch(bulkInitURL, {
+        headers: { Authorization: authValues },
+        method: 'POST',
+      });
+      if (!res.ok) {
+        throw new Error(
+          `Failure response to open elastic bulk posting: ${res.status}/${res.statusText}`
+        );
+      }
+    } catch (error) {
+      throw new VError(error, 'Open step of elastic bulk posting failed.');
+    }
+
+    var body;
+
+    try {
+      const uploadURL = elasticBaseURL + '/_bulk';
+      body = indexJSONLine + JSON.stringify(sentenceObj, null, 0) + '\n';
+
+      let res = await fetch(uploadURL, {
+        headers: {
+          Authorization: authValues,
+          'Content-Type': 'application/x-ndjson',
+        },
+        method: 'POST',
+        body,
+      });
+      if (!res.ok) {
+        throw new Error(
+          `Failure response to elastic sentence upload: ${res.status}/${res.statusText}`
+        );
+      }
+
+      const resText = await res.text();
+      if (res.ok) {
+        console.log('Completed', body, res.status, resText);
+      } else {
+        throw new Error(`Bulk upload failed: ${res.status} | ${resText}`);
+      }
+    } catch (error) {
+      throw new VError(error, 'Bulk upload failed.');
     }
   } catch (error) {
-    throw new VError(error, 'Open step of elastic bulk posting failed.');
-  }
-
-  var body;
-
-  try {
-    const uploadURL = elasticBaseURL + '/_bulk';
-    body = indexJSONLine + JSON.stringify(sentenceObj, null, 0) + '\n';
-
-    let res = await fetch(uploadURL, {
-      headers: {
-        Authorization: authValues,
-        'Content-Type': 'application/x-ndjson',
-      },
-      method: 'POST',
-      body,
-    });
-    if (!res.ok) {
-      throw new Error(
-        `Failure response to elastic sentence upload: ${res.status}/${res.statusText}`
-      );
+    // This gets us more detail than letting the error get caught outside this
+    // function. AWS won't log the cause property on its own, for example.
+    console.log(
+      'Error caught at the top level in sentences-to-elastic:',
+      error,
+      error.stack
+    );
+    if (error.jse_cause) {
+      console.log('jse_cause', JSON.stringify(error.jse_cause, null, 2));
     }
-  } catch (error) {
-    throw new VError(error, 'Bulk upload failed.');
+    throw error;
   }
-
-  // logForTracking({
-  //   message: 'Completed',
-  //   lambdaName,
-  //   filename,
-  //   jobId,
-  //   extra: {
-  //     elasticRequestBody: body,
-  //   },
-  // });
 }
 
 module.exports = { handler };
