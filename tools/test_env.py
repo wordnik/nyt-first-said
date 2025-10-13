692a282c829131836dcd04a8f442202de05c241a
Jim Kang <jimkang@gmail.com>
2025-10-13 16:42:44 -0400

    Run script from action.

---
+++ tools/test_env.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+import os
+from dotenv import load_dotenv
+import boto3
+
+load_dotenv()
+s3 = boto3.client("s3")
+
+print(f"S3A length: {len(os.getenv('S3A'))}, Wordnik key length: {len(os.getenv('WORDNIK_API_KEY'))}")
+
+s3.put_object(Bucket="nyt-said-sentences", Key="_a_test.txt",
+              Body="Hey", ContentType="text/plain")
