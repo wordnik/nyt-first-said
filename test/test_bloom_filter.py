e3ee2e66a0bd0c61cc85707755cfdb6705957da4
Jim Kang <jimkang@gmail.com>
2025-11-03 15:33:03 -0500

    Fix Bloom filter size calculation. Document.

--- test/test_bloom_filter.py
+++ test/test_bloom_filter.py
@@ -20,5 +20,17 @@ class BloomFilterSuite(unittest.TestCase):
     self.assertTrue(loaded_bloom_filter.contains("apple"))
     self.assertFalse(loaded_bloom_filter.contains("orange"))
 
+  def test_prebuilt(self):
+    bloom_filter = BloomFilter(size=26576494, num_hashes=10)
+    bloom_filter.load("data/bloom_filter.bits")
+
+    is_there = ["apple", "orange", "#OccupyYourNeighborhood", "rizzle", "yougotdunkedon"]
+    for word in is_there:
+        self.assertTrue(bloom_filter.contains(word))
+
+    not_there = ["?????aoeuaoeuaoeuasoethusar", "urobiome", "mommune", "rizzless", "wozzle"]
+    for word in not_there:
+      self.assertFalse(bloom_filter.contains(word))
+
 if __name__ == '__main__':
     unittest.main()
--- tools/build-bloom-filter.py
+++ tools/build-bloom-filter.py
@@ -12,7 +12,7 @@ ln_2 = 0.693147181 # ln(2)
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", type=str, required=True,
                     help="Path to the JSON file with a word list under the key `words`")
-parser.add_argument("-l", "--false_positive_limit", type=float, default=.01)
+parser.add_argument("-l", "--false_positive_limit", type=float, default=.001)
 parser.add_argument("-o", "--out", type=str, required=True)
 args = parser.parse_args()
 
@@ -26,11 +26,18 @@ with open(args.file, "r") as f:
 print(f"Word count: {len(word_list)}")
 
 # https://en.wikipedia.org/wiki/Bloom_filter#Optimal_number_of_hash_functions
-hash_count = round(-2.08 * ln(args.false_positive_limit))
-print(f"Hash count: {hash_count}")
+bits_per_word = round(-2.08 * ln(args.false_positive_limit))
+hash_count = round(-ln(args.false_positive_limit)/ln(2))
+filter_size = bits_per_word * len(word_list)
 
-bloom_filter = BloomFilter(len(word_list), hash_count)
+print(f"Bits per word: {bits_per_word}, Filter size: {filter_size}, Hash count: {hash_count}")
+print(f"When reconstituting, construct the bloom filter like this: BloomFilter(size={filter_size}, num_hashes={hash_count})")
+
+bloom_filter = BloomFilter(filter_size, hash_count)
 for word in word_list:
   bloom_filter.add(word)
 
 bloom_filter.save(args.out)
+
+# print(f"Has ?????aoeuaoeuaoeuasoethusar: {bloom_filter.contains('?????aoeuaoeuaoeuasoethusar')}")
+# print(f"Has dog: {bloom_filter.contains('dog')}")
