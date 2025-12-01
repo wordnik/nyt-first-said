40c0bf3dd93083bd63b2a885df236a23652ccbe8
Jim Kang <jimkang@gmail.com>
2025-11-30 21:54:49 -0500

    Fix local runs.

--- tools/scrape-site.sh
+++ tools/scrape-site.sh
@@ -3,4 +3,6 @@
 SITE=$1
 CURRENTDATE=$(/bin/date +%Y-%m-%d)
 
-python simple_scrape.py ${SITE} > meta/$SITE-run-$CURRENTDATE.txt 2> meta/$SITE-error-$CURRENTDATE.txt
+GITHUB_STEP_SUMMARY=meta/$SITE-summary-$CURRENTDATE.txt python simple_scrape.py ${SITE} > meta/$SITE-run-$CURRENTDATE.txt 2> meta/$SITE-error-$CURRENTDATE.txt
+
+echo $GITHUB_STEP_SUMMARY
