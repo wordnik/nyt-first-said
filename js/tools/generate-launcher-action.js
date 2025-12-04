0c6f949ca4efe89ada90b8777481f9e5110a1ce2
Jim Kang <jimkang@gmail.com>
2025-12-04 12:45:52 -0500

    Allow generated workflows to have different names.

--- js/tools/generate-launcher-action.js
+++ js/tools/generate-launcher-action.js
@@ -6,26 +6,30 @@ var yaml = require('js-yaml');
 
 if (process.argv.length < 3) {
   console.error(
-    'Usage: node js/tools/generate-launcher-action.js data/target-sites.json <includeWorks=true> .github/workflows/daily_launcher.yml'
+    'Usage: node js/tools/generate-launcher-action.js data/target-sites.json <includeWorks=true> <name=Daily launcher> .github/workflows/daily_launcher.yml'
   );
   process.exit(1);
 }
-
-const baseYAML = `name: Daily launcher
-on:
-  workflow_dispatch:
-  schedule:
-    - cron: '0 4 * * *'
-`;
 
 const sitesPath = process.argv[2];
 var sitesText = fs.readFileSync(sitesPath, { encoding: 'utf8' });
 var sites = JSON.parse(sitesText);
 var targetWorksStatus = true;
+var name = 'Daily launcher';
 
 if (process.argv.length > 3) {
   targetWorksStatus = JSON.parse(process.argv[3]);
 }
+if (process.argv.length > 4) {
+  name = process.argv[4];
+}
+
+const baseYAML = `name: ${name}
+on:
+  workflow_dispatch:
+  schedule:
+    - cron: '0 4 * * *'
+`;
 var jobs = {};
 
 for (let siteName in sites) {
