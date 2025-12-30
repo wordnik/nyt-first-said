03dbd3da923106454239a1cb8e29134b80ad8422
Jim Kang <jimkang@gmail.com>
2025-12-30 12:25:34 -0500

    Because the results of launcher action with 1000+ subactions cannot be loaded in GitHub, break the launchers up into multiple launchers with 500 sites each.

--- js/tools/generate-launcher-action.js
+++ js/tools/generate-launcher-action.js
@@ -6,61 +6,78 @@ var yaml = require('js-yaml');
 
 if (process.argv.length < 3) {
   console.error(
-    'Usage: node js/tools/generate-launcher-action.js data/target-sites.json <includeWorks=true> <name=Daily launcher> .github/workflows/daily_launcher.yml'
+    'Usage: node js/tools/generate-launcher-action.js data/target-sites.json <includeWorks=true> <name=Daily launcher>'
   );
   process.exit(1);
 }
 
 const sitesPath = process.argv[2];
 var sitesText = fs.readFileSync(sitesPath, { encoding: 'utf8' });
-var sites = JSON.parse(sitesText);
+var allSites = JSON.parse(sitesText);
 var targetWorksStatus = true;
-var name = 'Daily launcher';
+const maxSitesInLauncher = 500;
 
 if (process.argv.length > 3) {
   targetWorksStatus = JSON.parse(process.argv[3]);
 }
+var name = 'Daily launcher';
 if (process.argv.length > 4) {
   name = process.argv[4];
 }
 
-const baseYAML = `name: ${name}
-on:
-  workflow_dispatch:
-  schedule:
-    - cron: '0 4 * * *'
+var allSiteNames = Object.keys(allSites);
+for (
+  let launcherIndex = 0;
+  launcherIndex < Math.ceil(allSiteNames.length / maxSitesInLauncher);
+  ++launcherIndex
+) {
+  const siteStartIndex = launcherIndex * maxSitesInLauncher;
+  let siteNames = allSiteNames.slice(
+    siteStartIndex,
+    siteStartIndex + maxSitesInLauncher
+  );
+  const baseYAML = `name: ${name} ${launcherIndex}
+  on:
+    workflow_dispatch:
+    schedule:
+      - cron: '0 ${(17 + launcherIndex) % 24} * * *'
 `;
-var jobs = {};
 
-for (let siteName in sites) {
-  let site = sites[siteName];
-  if (site.skip) {
-    continue;
-  }
+  var jobs = {};
 
-  if (targetWorksStatus) {
-    if (!site.works) {
+  for (let siteName of siteNames) {
+    let site = allSites[siteName];
+    if (site.skip) {
       continue;
     }
-  } else {
-    // Skip if works is falsy, not just false.
-    if (site.works) {
-      continue;
+
+    if (targetWorksStatus) {
+      if (!site.works) {
+        continue;
+      }
+    } else {
+      // Skip if works is falsy, not just false.
+      if (site.works) {
+        continue;
+      }
     }
+
+    var id = siteName.replace(/\./g, '_');
+    if (/^\d/.test(id)) {
+      id = '_' + id;
+    }
+    jobs[id] = {
+      uses: 'wordnik/nyt-first-said/.github/workflows/brush.yml@master',
+      secrets: 'inherit',
+      with: {
+        site: siteName,
+      },
+    };
   }
 
-  var id = siteName.replace(/\./g, '_');
-  if (/^\d/.test(id)) {
-    id = '_' + id;
-  }
-  jobs[id] = {
-    uses: 'wordnik/nyt-first-said/.github/workflows/brush.yml@master',
-    secrets: 'inherit',
-    with: {
-      site: siteName,
-    },
-  };
+  const dailyLauncherYaml = baseYAML + yaml.dump({ jobs });
+  const yamlPath = `.github/workflows/daily_launcher_${launcherIndex
+    .toString()
+    .padStart(4, '0')}.yml`;
+  fs.writeFileSync(yamlPath, dailyLauncherYaml, { encoding: 'utf8' });
 }
-
-const dailyLauncherYaml = baseYAML + yaml.dump({ jobs });
-console.log(dailyLauncherYaml);
