3fef2e017e580bd732c9885843af13a7dfddff6b
Jim Kang <jimkang@gmail.com>
2026-01-13 13:48:53 -0500

    Handle more forms of hidden characters.

--- test/test_text_cleaning.py
+++ test/test_text_cleaning.py
@@ -6,8 +6,14 @@ class TestCleaningSuite(unittest.TestCase):
         self.assertEqual(remove_punctuation("Joe’s"), "Joe", "Special apostrophe removed.")
 
     def test_clean_trouble_chars(self):
+        self.maxDiff = None
         self.assertEqual(remove_trouble_characters("\u200Dhas\u200D"), "has", "Zero-width joiner removed.")
         self.assertEqual(remove_trouble_characters("to\u00A0"), "to", "Non-breaking space removed.")
+        self.assertEqual(remove_trouble_characters(
+            "The stores earmarked for \xe2\x80\x8bclosure from February 2 include one in suburban Shanghai, another in Guangzhou, and \xe2\x81\xa0several more in second-tier Chinese cities such as Nantong, Xuzhou and Harbin, IKEA said in a post on its official WeChat account."
+        ),
+                         "The stores earmarked for closure from February 2 include one in suburban Shanghai, another in Guangzhou, and several more in second-tier Chinese cities such as Nantong, Xuzhou and Harbin, IKEA said in a post on its official WeChat account.",
+                         "Non-breaking space removed.")
 
     def test_has_username(self):
         self.assertEqual(has_username("@ausmencricket"), True, "@-based username found.")
