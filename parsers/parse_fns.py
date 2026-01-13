0596acd054605e033dc480ac2e3cd429228e8cf4
Jim Kang <jimkang@gmail.com>
2026-01-13 14:22:58 -0500

    Add article_based parser test.

--- parsers/parse_fns.py
+++ parsers/parse_fns.py
@@ -36,9 +36,6 @@ def p_tags_to_body(p_tags):
 # parser_fns need to take an html string and return an object with body and meta keys and, optionally, the soup instance.
 
 def article_based(html, get_additional_p_tags = None):
-    # print("Working on html")
-    # print(html)
-
     # if it's not a str, decode it:
     if not isinstance(html, str):
         try:
