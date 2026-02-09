f5ec63c61d28a5687c9d0bb6c16fe7e29d5a9a2f
Jim Kang <jimkang@gmail.com>
2026-02-09 18:45:33 -0500

    Log diagnostic information when there is a missing browser executable.

--- utils/headless.py
+++ utils/headless.py
@@ -1,5 +1,6 @@
-from playwright.sync_api import sync_playwright
 import logging
+import os
+from playwright.sync_api import sync_playwright
 
 playwright = sync_playwright().start()
 
@@ -7,6 +8,21 @@ playwright = sync_playwright().start()
 class HeadlessBrowser():
     def __init__(self):
         launcher = playwright.firefox
+        exe_path = launcher.executable_path #+ "test"
+        browser_exe_exists = os.access(exe_path, os.X_OK)
+        print(f"Is browser at {exe_path} there and executable? {browser_exe_exists}")
+        if not browser_exe_exists:
+            parent_path = os.path.dirname(exe_path)
+            for i in range(3):
+                parent_exists = os.access(parent_path, os.F_OK)
+                print(f"{parent_path} exists?", parent_exists)
+                if parent_exists:
+                    print(f"Contents of {parent_path}: {os.listdir(parent_path)}")
+                    break
+                parent_path = os.path.dirname(parent_path)
+
+            raise OSError(f"Browser not found or not executable at {launcher.executable_path}.")
+
         self.browser = launcher.launch()
         self.context = None
         self.page = None
