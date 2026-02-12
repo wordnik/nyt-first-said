3ebe9ee347761b90dfa25a9fc7c339be635fe53c
Jim Kang <jimkang@gmail.com>
2026-02-11 20:23:12 -0500

    Add script for filling missing urls with search results.

---
+++ tools/fill_missing_urls.py
@@ -0,0 +1,28 @@
+# import sys
+# sys.path.append('.')
+from search import search
+import json
+import os
+
+# print(search("unh"))
+
+with open("data/target_sites.json", "r") as f:
+    sites_dict = json.loads(f.read())
+    f.close()
+
+count = 0
+for k, site_obj in sites_dict.items():
+    feeder_pages = site_obj["feeder_pages"]
+    if len(feeder_pages) == 1 and feeder_pages[0] == "https://":
+        # print(site_obj["site"])
+        feeder_pages[0] = search(site_obj["site"])
+        print(site_obj)
+        count +=1
+        if count > 100:
+            break
+        # Paranoid write after every url found.
+        with open("data/target_sites.json", "w") as f:
+            f.write(json.dumps(sites_dict, indent=2))
+            f.close()
+
+print(f"{count} urls filled.")
---
+++ tools/search.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('.')
+from utils.headless import HeadlessBrowser 
+
+browser = HeadlessBrowser()
+
+def search(term):
+    page = browser.get_page("https://html.duckduckgo.com/html/?q=" + term)
+    result = page.locator("#links .web-result").nth(0)
+    link = result.locator(".result__title a")
+    outbound_url = link.get_attribute("href")
+    print(outbound_url)
+    link.click()
+    page.wait_for_load_state(timeout = 60000)
+    # page.wait_for_url("(?!https:\/\/duckduckgo\.com)")
+    return page.url
+
